40e41a01ddc2f8ef7bf502454f6b86924a6a6602
mogit2020 <mohibullah.kamal@gmail.com>
2019-11-26 14:53:16 +0100

    add GET and Post request

---
+++ demo/src/main/java/com/mohib/demo/HelloController.java
@@ -0,0 +1,79 @@
+package com.mohib.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+// this is an annotation... in Angular we use decorators...
+@RestController
+@RequestMapping("/greeting")
+public class HelloController {
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String sayHello() {
+		return "hello world <h1>Html also works; that is awesome...</h1>";
+	}
+
+// //****************************************************************************
+// //************ This is for POST request **************************************
+	
+//	@RequestMapping("/user_entry") 
+//	public String userForm() {
+//		return 
+//		"<form action=\"/greeting/user_greeting\" method=\"POST\">\n" + 
+//		"  First name:<br>\n" + 
+//		"  <input type=\"text\" name=\"firstname\" value=\"Mickey\">\n" + 
+//		"  <br>\n" + 
+//		"  Last name:<br>\n" + 
+//		"  <input type=\"text\" name=\"lastname\" value=\"Mouse\">\n" + 
+//		"  <br><br>\n" + 
+//		"  <input type=\"submit\" value=\"Submit\">\n" + 
+//		"</form>";
+//	}
+//	
+//	@RequestMapping(value="/user_greeting", method = RequestMethod.POST)
+//	public String printUserGreeting(@RequestParam String firstname, @RequestParam String lastname) {
+//		return "Form submitted..." + 
+//				"<br>" + "Hello there, " + 
+//				firstname + " " + lastname + 
+//				". How are you?....";
+//	}
+// //****************************************************************************
+// //****************************************************************************
+
+
+	
+// //****************************************************************************
+// //************ This is for GET request for Query Parameter... ****************
+
+	@RequestMapping("/user_entry") 
+	public String userForm() {
+		return 
+		"<form action=\"/greeting/user_greeting\" method=\"GET\">\n" + 
+		"  First name:<br>\n" + 
+		"  <input type=\"text\" name=\"firstname\" value=\"Mickey\">\n" + 
+		"  <br>\n" + 
+		"  Last name:<br>\n" + 
+		"  <input type=\"text\" name=\"lastname\" value=\"Mouse\">\n" + 
+		"  <br><br>\n" + 
+		"  <input type=\"submit\" value=\"Submit\">\n" + 
+		"</form>";
+	}
+	
+	@RequestMapping(value="/user_greeting", method = RequestMethod.GET)
+	public String printUserGreeting(@RequestParam String firstname, @RequestParam String lastname) {
+		return "Form submitted..." + 
+				"<br>" + "Hello there, " + 
+				firstname + " " + lastname + 
+				". How are you?....";
+	}
+// //****************************************************************************
+// //****************************************************************************
+
+	
+}
+
+
+
+
